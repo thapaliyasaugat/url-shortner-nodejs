6b3c4c8a7c9c556cd149e4954bad079fb6dfa9a8
Your Name <thapaliyasaugat99@gmail.com>
2022-04-12 09:48:48 +0545

    start writting test

--- backend/controller/__test__/url.test.ts
+++ backend/controller/__test__/url.test.ts
@@ -1,7 +1,19 @@
+import { MetadataAlreadyExistsError } from "typeorm";
 import urlController from "../url.controller";
-
+import axios from 'axios'
 describe('url service', () => {
     it("it should pass", async () => {
         expect(true).toBe(true);
     });
+
+    it('get all record', async () => {
+        const apiUrl = "http://localhost:5000/shorten";
+        await axios.get(apiUrl)
+            .then(res => {
+                expect(res.status).toBe(200);
+                expect(res.data).toBeDefined();
+            }).catch(err => {
+                throw new Error('should be sucessfull')
+            })
+    })
 })
